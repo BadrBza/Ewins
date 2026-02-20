3be4ca0cb775cea18c6830cdb7af4be25ed6fca5
Badr <144738472+BadrBza@users.noreply.github.com>
2026-02-20 23:18:12 +0100

    Update BD.php

--- inc/BD.php
+++ inc/BD.php
@@ -1,27 +1,26 @@
 <?php
+
 namespace inc;
 
-class BD {
-    private $host = '192.168.132.203';
-    private $username = 'Q220251';
-    private $password = '7f91b28807eaf65138b621063d4015e616364286';
+class BD
+{
+    private $host = '127.0.0.1';
+    private $username = 'root';
+    private $password = '';
     private $database = 'Q220251';
     public $conn;
 
-  
-
-    public function __construct() {
+    public function __construct()
+    {
         $this->conn = new \mysqli($this->host, $this->username, $this->password, $this->database);
-
 
         if ($this->conn->connect_error) {
             die("Connection failed: " . $this->conn->connect_error);
         }
     }
 
-    public function query($sql) {
+    public function query($sql)
+    {
         return $this->conn->query($sql);
     }
 }
-
-?>
